21d6d21d91eea7dbfb30013134609afc6ffd6fbb
ici906 <ici906@DESKTOP-P4JGH6N>
2023-01-18 17:38:11 +0900

    2023-01-18 최종 완료

--- src/main/java/com/joonyoung/home/JoonProjectApplication.java
+++ src/main/java/com/joonyoung/home/JoonProjectApplication.java
@@ -2,12 +2,20 @@ package com.joonyoung.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JoonProjectApplication {
+public class JoonProjectApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(JoonProjectApplication.class, args);
-	}
+	   public static void main(String[] args) {
+	      SpringApplication.run(JoonProjectApplication.class, args);
+	   }
 
+	   @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      // TODO Auto-generated method stub
+	      return builder.sources(JoonProjectApplication.class);
+	   }
+	   
 }
